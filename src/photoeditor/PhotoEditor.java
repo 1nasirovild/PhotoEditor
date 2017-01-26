b6a4c148c5db969a2fdc3d5d05bec6b19abbd684
Ildar Nasirov <ildar.nasirov1@gmail.com>
2017-01-26 11:13:32 -0600

    - Version 1.0

---
+++ src/photoeditor/PhotoEditor.java
@@ -0,0 +1,22 @@
+/*
+ * Name: Ildar Nasirov
+ * Project Name: Photo Editor
+ * Version: 1.0
+*/
+
+package photoeditor;
+
+/**
+ *
+ * @author ildar
+ */
+public class PhotoEditor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
